d87a886384f855e375fa57407cf278f0463adb8b
Michael Cannon <mc@aihr.us>
2014-12-18 18:42:58 -0500

    Rework search video only results handling

--- functions/user/custom/lib/class.search_video_widget.php
+++ functions/user/custom/lib/class.search_video_widget.php
@@ -6,7 +6,7 @@
  */
 
 class Search_Video_Widget extends WP_Widget {
-	public $field				= 'fitv_show_only_posts_videos';
+	public $field = 'fitv_show_only_posts_videos';
 
 	/**
 	 * Register widget with WordPress.
@@ -38,8 +38,9 @@ class Search_Video_Widget extends WP_Widget {
 
 		echo $before_widget;
 
-		if ( ! empty( $title ) )
+		if ( ! empty( $title ) ) {
 			echo $before_title . $title . $after_title;
+		}
 
 		echo $this->search_videos();
 
@@ -48,24 +49,24 @@ class Search_Video_Widget extends WP_Widget {
 
 	public function update_var() {
 		if ( isset( $_POST[ $this->field ] ) ) {
-			$value				= $_POST[ $this->field ];
+			$value = $_POST[ $this->field ];
 			update_option( $this->field, $value );
 		}
 	}
 
 	private function search_videos() {
 		// create form
-		$this_url				= curPageURL();
-		$label					= __( 'Only show posts with videos', 'fitv' );
-		$label_yes				= __( 'Yes', 'fitv' );
-		$label_no				= __( 'No', 'fitv' );
+		$this_url  = curPageURL();
+		$label     = __( 'Only show posts with videos', 'fitv' );
+		$label_yes = __( 'Yes', 'fitv' );
+		$label_no  = __( 'No', 'fitv' );
 		// show checkbox "only show posts with videos"
 		// set checked or not
-		$option					= get_option( $this->field, 0 );
-		$checked_yes			= checked( $option, 1, false );
-		$checked_no				= checked( $option, 0, false );
+		$option	     = get_option( $this->field, 0 );
+		$checked_yes = checked( $option, 1, false );
+		$checked_no  = checked( $option, 0, false );
 
-		$form					= <<<EOD
+		$form = <<<EOD
 			<form action="{$this_url}" method="post">
 				<label for="{$this->field}">
 					{$label}<br />
@@ -92,8 +93,8 @@ EOD;
 	 * @return array Updated safe values to be saved.
 	 */
 	public function update( $new_instance, $old_instance ) {
-		$instance				= array();
-		$instance['title']		= strip_tags( $new_instance['title'] );
+		$instance          = array();
+		$instance['title'] = strip_tags( $new_instance['title'] );
 
 		return $instance;
 	}
@@ -107,10 +108,9 @@ EOD;
 	 */
 	public function form( $instance ) {
 		if ( isset( $instance[ 'title' ] ) ) {
-			$title				= $instance[ 'title' ];
-		}
-		else {
-			$title				= __( 'Video Posts', 'fitv' );
+			$title = $instance[ 'title' ];
+		} else {
+			$title = __( 'Video Posts', 'fitv' );
 		}
 ?>
 		<p>
@@ -122,6 +122,7 @@ EOD;
 
 } // class Search_Video_Widget
 
+
 function fitv_search_video_widget() {
 	register_widget( 'Search_Video_Widget' );
 }
@@ -129,43 +130,14 @@ function fitv_search_video_widget() {
 if ( true ) {
 	add_action( 'init', 'fitv_init_get_search_form' );
 	add_action( 'widgets_init', 'fitv_search_video_widget' );
-	add_filter( 'pre_get_posts', 'fitv_video_posts_filter' );
-	// add_filter( 'posts_where', 'fitv_video_posts_where' );
-	add_filter( 'relevanssi_where', 'fitv_video_posts_relevanssi_where' );
-}
-
-function fitv_video_posts_filter( $query ) {
-	if ( is_admin() || ! $query->is_search )
-		return;
-
-	global $wpdb;
-
-	$do_it						= get_option( 'fitv_show_only_posts_videos' );
-
-	if ( $do_it ) {
-		$query->query_vars['post_type']	= array( 'video' );
-
-		$query         = "SELECT p.ID FROM {$wpdb->posts} p LEFT JOIN {$wpdb->postmeta} pm ON p.ID = pm.post_id WHERE p.post_status = 'publish' AND pm.meta_key = 'wpzoom_post_embed_code' AND pm.meta_value != ''";
-		// $query         = "SELECT post_id FROM {$wpdb->postmeta} WHERE meta_key = 'wpzoom_post_embed_code' AND meta_value != ''";
-		$fitv_post_ids = $wpdb->get_col( $query );
-
-		if ( empty( $fitv_post_ids ) )
-			return;
-
-		// fails for Relevanssi and other search plugin tie-ins
-		$query->query_vars['post__in']	= $fitv_post_ids;
-
-		// use posts_where then as failsafe
-		update_option( 'fitv_show_only_posts_videos_values', $fitv_post_ids );
-	}
+	add_filter( 'relevanssi_results', 'fitv_relevanssi_results' );
 }
 
 
 function fitv_init_get_search_form() {
-	$field						= 'fitv_show_only_posts_videos';
-
+	$field = 'fitv_show_only_posts_videos';
 	if ( isset( $_REQUEST[ $field ] ) ) {
-		$value					= intval( $_REQUEST[ $field ] );
+		$value = intval( $_REQUEST[ $field ] );
 		update_option( $field, $value );
 	} else {
 		update_option( $field, 0 );
@@ -173,59 +145,54 @@ function fitv_init_get_search_form() {
 }
 
 
-function fitv_video_posts_relevanssi_where( $where ) {
-	if ( is_admin() || ! is_search() )
-		return $where;
-
-	$do_it						= get_option( 'fitv_show_only_posts_videos' );
-	$posts						= get_option( 'fitv_show_only_posts_videos_values' );
-
-	if ( $do_it && ! empty( $posts ) ) {
-		$where					.= ' AND doc IN ( ' . implode( ',', $posts ) . ' ) ';
-		// global $wpdb;
-		// $query  = "SELECT post_id FROM {$wpdb->postmeta} WHERE meta_key = 'wpzoom_post_embed_code' AND meta_value != ''";
-		// $where .= ' AND doc IN ( ' . $query . ' ) ';
-	}
-
-	return $where;
-}
-
-
-function fitv_video_posts_where( $where ) {
-	if ( is_admin() || ! is_search() )
-		return $where;
-
-	$do_it						= get_option( 'fitv_show_only_posts_videos' );
-	$posts						= get_option( 'fitv_show_only_posts_videos_values' );
-
-	if ( $do_it && ! empty( $posts ) ) {
-		$where					.= ' AND ID IN ( ' . implode( ',', $posts ) . ' ) ';
-	}
-
-	return $where;
-}
-
-
 function fitv_get_search_form( $form ) {
-	$field						= 'fitv_show_only_posts_videos';
-	$label						= __( 'Only show posts with videos', 'fitv' );
-	$label_yes					= __( 'Yes', 'fitv' );
-	$label_no					= __( 'No', 'fitv' );
+	$field     = 'fitv_show_only_posts_videos';
+	$label     = __( 'Only show posts with videos', 'fitv' );
+	$label_yes = __( 'Yes', 'fitv' );
+	$label_no  = __( 'No', 'fitv' );
 	// show checkbox "only show posts with videos"
 	// set checked or not
-	$option						= get_option( $field, 0 );
-	$checked_yes				= checked( $option, 1, false );
+	$option      = get_option( $field, 0 );
+	$checked_yes = checked( $option, 1, false );
 
-	$find						= '<input type="submit" id="searchsubmit" value="Search" />';
-	$replace					= <<<EOD
+	$find    = '<input type="submit" id="searchsubmit" value="Search" />';
+	$replace = <<<EOD
 		<label for="{$field}">
 			{$label}
 		</label>
 		<input type="checkbox" name="{$field}" value="1" {$checked_yes} />
 EOD;
-	$form						= str_replace( $find, $find . $replace, $form );
+
+	$form = str_replace( $find, $find . $replace, $form );
 
 	return $form;
+}
+
+
+function fitv_relevanssi_results( $results ) {
+	$do_it = get_option( 'fitv_show_only_posts_videos' );
+	if ( empty( $do_it ) || empty( $results ) ) {
+		return $results;
+	}
+
+	global $wpdb;
+
+	$found = array_keys( $results );
+	$sql   = "SELECT p.ID FROM {$wpdb->posts} p LEFT JOIN {$wpdb->postmeta} pm ON p.ID = pm.post_id WHERE pm.meta_key = 'wpzoom_post_embed_code' AND pm.meta_value != ''";
+	$sql  .= ' AND p.ID IN (' . implode( ',', $found ) . ')';
+	$posts = $wpdb->get_col( $sql );
+	if ( empty( $posts ) ) {
+		return $results;
+	}
+
+	// remove all but $posts from $results, done here than in query due to reduce db load
+	foreach ( $results as $key => $value ) {
+		if ( ! in_array( $key, $posts ) ) {
+			unset( $results[ $key ] );
+		}
+	}
+
+	return $results;
 }
 
 ?>
